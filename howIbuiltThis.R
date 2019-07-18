2f16827edfdde45a7700d8fba292e338b8f36c66
Amy Paguirigan <amypaguirigan@gmail.com>
2019-07-18 12:34:53 -0700

    test this

---
+++ howIbuiltThis.R
@@ -0,0 +1,16 @@
+library(usethis)
+# Modify the description ----------------------------------------------
+
+use_gpl3_license("FHR")
+
+# Set up various packages ---------------------------------------------
+use_roxygen_md()
+
+?use_revdep()
+# Set up other files -------------------------------------------------
+use_readme_md()
+
+use_news_md()
+
+# Use git ------------------------------------------------------------
+use_git()
