7a46616be12945f5711ca9eb2beb8013a6324d19
Amy Paguirigan <amypaguirigan@gmail.com>
2019-07-18 14:57:57 -0700

    how I built this.

--- howIbuiltThis.R
+++ howIbuiltThis.R
@@ -1,16 +1,17 @@
 library(usethis)
 # Modify the description ----------------------------------------------
-
 use_gpl3_license("FHR")
 
 # Set up various packages ---------------------------------------------
 use_roxygen_md()
 
-?use_revdep()
 # Set up other files -------------------------------------------------
 use_readme_md()
-
 use_news_md()
 
 # Use git ------------------------------------------------------------
 use_git()
+
+## Then I created a FredHutch repository and set up the remote repository
+## via the command line though I personally use Git Kracken to manage git
+## repositories (Amy Paguirigan)
